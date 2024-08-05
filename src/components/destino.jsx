c100907894a2039489a3d2aeef971d2383590623
EDUARDO BORGES CAMBRAIA <eduardo_cambraia@estudante.sesisenai.org.br>
2024-08-05 08:53:49 -0300

    Melhorias Visuais

--- src/components/destino.jsx
+++ src/components/destino.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Destino = ({ nome, descricao }) => {
     return (
-        <div>
+        <div className="destino">
             <h2>{nome}</h2>
             <p>{descricao}</p>
         </div>
